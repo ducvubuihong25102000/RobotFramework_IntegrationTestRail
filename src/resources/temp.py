ab39095cea7f3fe90ada5107258669264ce58131
Vũ Bùi Hồng Đức <75364542+ducvubuihong25102000@users.noreply.github.com>
2025-10-13 22:37:52 +0700

    check utilzation

---
+++ src/resources/temp.py
@@ -0,0 +1,13 @@
+import os
+
+__version__ = '1.0.0'
+class temp:
+    ROBOT_LIBRARY_VERSION = __version__
+    ROBOT_LIBRARY_SCOPE = 'SUITE'
+    
+    def __init__(self):
+        self.conn = None
+
+    def get_env_variable(self, var_name):
+        test_run = os.getenv(var_name)
+        print(f"Test Run ID: {test_run}")
---
+++ src/tests/temp.robot
@@ -0,0 +1,10 @@
+*** Settings ***
+Library    ../resources/temp.py    WITH NAME    TempLib
+
+*** Variables ***
+
+
+*** Test Cases ***
+1. Verify Pipeline 1 happy case
+    [Tags]    TC_141
+    TempLib.Get Env Variable    var_name="test_run_id"
